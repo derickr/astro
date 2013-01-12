e3b65db020db9d6967ec06deba525f48a49aa741
Derick Rethans <github@derickrethans.nl>
2013-01-12 14:59:50 +0000

    Added a bunch of comments and refactored some code for easier reuse.

--- astro.c
+++ astro.c
@@ -11,14 +11,18 @@
 #define atand(x)    (RADEG*atan(x))
 #define atan2d(y,x) (RADEG*atan2((y),(x)))
 
-
-double rev( double x )
+/* Limits the angle "x" to 360 degrees */
+static double rev( double x )
 {
 	return  x - floor( x / 360.0 ) * 360.0;
 }
 
-
-double cbrt( double x )
+#ifndef HAVE_CBRT
+/* Calculates the cubic root of "x"
+ * in: x
+ * return: cubic root of "x"
+ */
+static double cbrt( double x )
 {
 	if ( x > 0.0 )
 	{
@@ -33,8 +37,11 @@ double cbrt( double x )
 		return 0.0;
 	}
 }
+#endif
 
-/*
+/* Converts spherical coordinates ra, decl and r to rectangular coordinates x,
+ * y, and z
+ *
  * in:   ra and decl in degrees, r in "units"
  * out:  x, y, z in "units"
  */
@@ -45,7 +52,9 @@ void spherical_to_rectangular(double ra, double decl, double r, double *x, doubl
 	*z = r * sind(decl);
 }
 
-/*
+/* Converts rectangular coordinates x, y and z to spherical coordinates ra,
+ * decl and r
+ *
  * in:  x, y, z in "units"
  * out: ra and decl in degrees, r in "units"
  */
@@ -56,19 +65,22 @@ void rectangular_to_spherical(double x, double y, double z, double *ra, double *
 	*decl = atan2d( z, sqrt( x * x + y * y ) );
 }
 
-
-/*
+/* Rotates the rectangular coordinates x, y, and z around the X-axis by oblecl
+ * degrees
+ *
  * in:  x, y, z in "units", oblecl in degrees
  * out: xr, yr, xr in "units"
  */
-static void rotate(double x, double y, double z, double oblecl, double *xr, double *yr, double *zr)
+static void rotatex(double x, double y, double z, double oblecl, double *xr, double *yr, double *zr)
 {
 	*xr = x;
 	*yr = y * cosd(oblecl) - z * sind(oblecl);
 	*zr = y * sind(oblecl) + z * cosd(oblecl);
 }
 
-/*
+/* Rotates the rectangular coordinates x, y, and z around the Y-axis by oblecl
+ * degrees
+ *
  * in:  x, y, z in "units", oblecl in degrees
  * out: xr, yr, xr in "units"
  */
@@ -79,39 +91,48 @@ static void rotatey(double x, double y, double z, double oblecl, double *xr, dou
 	*zr = x * sind(oblecl) + z * cosd(oblecl);
 }
 
-/*
+/* Converts ecliptic to equatorial coordinates by rotating around the X-axis
+ *
  * in:  x, y, z in "units", oblecl in degrees
  * out: xr, yr, xr in "units"
  */
 void ecliptic_to_equatorial(double x, double y, double z, double oblecl, double *xr, double *yr, double *zr)
 {
-	return rotate(x, y, z, oblecl, xr, yr, zr);
+	return rotatex(x, y, z, oblecl, xr, yr, zr);
 }
 
-/*
+/* Converts equatorial to ecliptic coordinates by reverse-rotating around the X-axis
+ *
  * in:  x, y, z in "units", oblecl in degrees
  * out: xr, yr, xr in "units"
  */
 void equatorial_to_ecliptic(double x, double y, double z, double oblecl, double *xr, double *yr, double *zr)
 {
-	return rotate(x, y, z, 0 - oblecl, xr, yr, zr);
+	return rotatex(x, y, z, 0 - oblecl, xr, yr, zr);
 }
 
-/*
- * in:  y, m, d
- * out: daynr
+/* Computes year, month, day, hour, minute and second to a "day number" from
+ * 2000 Jan 0.0
+ *
+ * in:  y, m, d, h, i, s
+ * out: time
  */
-void date_to_daynr(int y, int m, int d, int *daynr)
+void date_to_daynr(int y, int m, int d, int h, int i, int s, double *time)
 {
-	*daynr = 367 * y - (7 * (y + ((m+9)/12))) / 4 + (275 * m)/9 + d - 730530;
+	int daynr;
+
+	daynr = 367 * y - (7 * (y + ((m+9)/12))) / 4 + (275 * m)/9 + d - 730530;
+	*time = daynr + ((h + ((i + (s / 60)) / 60)) / 24);
 }
 
-
-void sunpos(int d, double *lon, double *ra, double *decl, double *rad)
+/* Calculates the Sun's Mean Longitude (L), Right Ascension (RA), Declination
+ * (decl) and "rad"
+ *
+ * in: d (time)
+ * out: L, RA, decl, rad */
+void sunpos(double d, double *L, double *ra, double *decl, double *rad)
 {
-	double w, a, e, M, oblecl, L, E, x, y, r, v, z, ex, ey, ez;
-	double GMST0, SIDTIME, HA;
-	double LONG, LAT, azimuth, altitude;
+	double w, a, e, M, oblecl, E, x, y, r, v, z, ex, ey, ez, i_lon;
 
 	w = 282.9404 + 4.70935e-5 * d;
 	a = 1.000000;
@@ -121,8 +142,8 @@ void sunpos(int d, double *lon, double *ra, double *decl, double *rad)
 
 	oblecl = 23.4393 - 3.563e-7 * d;
 
-	L = w + M;
-	L = rev(L);
+	*L = w + M;
+	*L = rev(*L); /* Sun's mean logitude */
 
 	E = M + (180/PI) * e * sind(M) * (1 + e * cosd(M));
 
@@ -132,51 +153,43 @@ void sunpos(int d, double *lon, double *ra, double *decl, double *rad)
 	r = sqrt(x*x + y*y);
 	v = atan2d(y, x);
 
-	*lon = rev(v + w);
+	i_lon = rev(v + w);
 
 	/* reusing x, y, and z here */
-	x = r * cosd(*lon);
-	y = r * sind(*lon);
+	x = r * cosd(i_lon);
+	y = r * sind(i_lon);
 	z = 0.0;
 
 	ecliptic_to_equatorial(x, y, z, oblecl, &ex, &ey, &ez);
 	rectangular_to_spherical(ex, ey, ez, ra, decl, rad);
-
-	LONG = -0.127;
-	LAT  = 51.50;
-
-	GMST0 = rev( L + 180 ); /* in degrees */
-	SIDTIME = GMST0 + ( 8.2 * 15 ) /* UT */ + LONG /* 15°E */;
-
-	HA = SIDTIME - *ra;
-
-	/* reusing x, y, and z here again */
-	spherical_to_rectangular(HA, *decl, 1, &x, &y, &z);
-	rotatey(x, y, z, 90 - LAT, &ex, &ey, &ez);
-
-	azimuth = atan2d( ey, ex ) + 180;
-	altitude = atan2d( ez, sqrt( ex*ex + ey*ey ) );
-
-	printf("%f, %f\n", azimuth, altitude);
 }
 
-
-
-int main(void)
+/* Calculates the Siderial Time (sidtime) and Hour Angle (HA)
+ * in: L, utoffset, lon, ra
+ * out: sidtime, ha
+ */
+void sidtime_and_ha(double L, double utoffset, double lon, double ra, double *sidtime, double *ha)
 {
-/*
-	double x, y, z, ex, ey, ez, ra, decl, r;
+	double GMST0;
 
-	spherical_to_rectangular(90, 0, 1, &x, &y, &z);
-	ecliptic_to_equatorial(x, y, z, 23.4, &ex, &ey, &ez);
-	rectangular_to_spherical(ex, ey, ez, &ra, &decl, &r);
+	/* Sidereal time and hour angle, Altitude and azimuth */
+	GMST0 = rev( L + 180 ); /* in degrees */
+	*sidtime = GMST0 + ( utoffset * 15 ) /* UT */ + lon /* 15°E */;
 
-	printf("%f, %f, %f\n", ra, decl, r);
-*/
-	int daynr;
-	double lon, ra, decl, r;
+	*ha = *sidtime - ra;
+}
 
-	date_to_daynr(2013, 1, 11, &daynr);
+/* Calculates the Sun's Altitude and Azimuth
+ * in: L, lat, ha, decl
+ * out: azimuth, altitude */
+void sunaltazimuth(double L, double lat, double ha, double decl, double *azimuth, double *altitude)
+{
+	double x, y, z;
+	double ex, ey, ez; /* temp vars */
 
-	sunpos(daynr, &lon, &ra, &decl, &r);
+	spherical_to_rectangular(ha, decl, 1, &x, &y, &z);
+	rotatey(x, y, z, 90 - lat, &ex, &ey, &ez);
+
+	*azimuth = atan2d( ey, ex ) + 180;
+	*altitude = atan2d( ez, sqrt( ex*ex + ey*ey ) );
 }
